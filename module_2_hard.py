00034b126c81c5cfc93e46b90161920634c9741c
Kott647 <apperkot2015@gmail.com>
2024-11-13 23:43:48 +0300

    Add files via upload

---
+++ module_2_hard.py
@@ -0,0 +1,42 @@
+import random
+def code():
+    numbers = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+    number1 = random.choice(numbers)
+    if number1 == 3:
+        print(number1, "-", 12)
+    if number1 == 4:
+        print(number1, "-", 13)
+    if number1 == 5:
+        print(number1, "-", 1423)
+    if number1 == 6:
+        print(number1, "-", 121524)
+    if number1 == 7:
+        print(number1, "-", 162534)
+    if number1 == 8:
+        print(number1, "-", 13172635)
+    if number1 == 9:
+        print(number1, "-", 1218273645)
+    if number1 == 10:
+        print(number1, "-", 141923283746)
+    if number1 == 11:
+        print(number1, "-", 11029384756)
+    if number1 == 12:
+        print(number1, "-",  12131511124210394857)
+    if number1 == 13:
+        print(number1, "-", 112211310495867)
+    if number1 == 14:
+        print(number1, "-",  1611325212343114105968)
+    if number1 == 15:
+        print(number1, "-", 1214114232133124115106978)
+    if number1 == 16:
+        print(number1, "-", 1317115262143531341251161079)
+    if number1 == 17:
+        print(number1, "-", 11621531441351261171089)
+    if number1 == 18:
+        print(number1, "-", 12151811724272163631545414513612711810)
+    if number1 == 19:
+        print(number1, "-", 118217316415514613712811910)
+    if number1 == 20:
+        print(number1, "-", 13141911923282183731746416515614713812911)
+code()
+
